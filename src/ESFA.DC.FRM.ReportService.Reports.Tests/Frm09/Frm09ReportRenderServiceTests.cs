46e8ba4226c4ab509041ceb7c7965ebbac402419
kelvin.morris <kelvin.morris@eas.co.uk>
2020-11-23 10:10:58 +0000

    #105177 adding tests for frm09

---
+++ src/ESFA.DC.FRM.ReportService.Reports.Tests/Frm09/Frm09ReportRenderServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+using Aspose.Cells;
+using ESFA.DC.FRM.ReportService.Reports.Model.Worksheets;
+using ESFA.DC.FRM.ReportService.Reports.Worksheets.FRM09;
+using Xunit;
+
+namespace ESFA.DC.FRM.ReportService.Reports.Tests.Frm09
+{
+    public class Frm09ReportRenderServiceTests
+    {
+        [Fact]
+        public void Render()
+        {
+            var workbook = new Workbook();
+            var worksheet = workbook.Worksheets[0];
+
+            var frm09ReportModels = Enumerable.Range(1, 5).Select(l => new Frm09ReportModel
+            {
+                LearnAimRef = l.ToString(),
+                UKPRN = 1000000 + l,
+                OrgName = $"Org{l}"
+            });
+
+            NewService().Render(frm09ReportModels, worksheet);
+
+            Directory.CreateDirectory("Output");
+            workbook.Save("Output/FRM09_FundingRuleMonitoringReport.xlsx");
+        }
+
+        private Frm09RenderService NewService()
+        {
+            return new Frm09RenderService();
+        }
+    }
+}
